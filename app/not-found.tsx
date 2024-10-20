d406fb617b09a579bcb555f2f87a18cab6c2d6f0
Roshan Kumar <rsahu7989@gmail.com>
2024-10-20 16:45:52 +0530

    Add NotFound component for handling 404 errors

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className='text-4xl font-extrabold w-screen pt-20 grid place-items-center'>
+            <h1>
+                404 - Page Not Found
+            </h1>
+        </div>
+    )
+}
+
+export default NotFound
