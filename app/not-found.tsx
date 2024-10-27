78d78bd372ada69241ae2029691690f7113ca18c
Roshan Kumar <rsahu7989@gmail.com>
2024-10-28 00:53:42 +0530

    Refactor: Add metadata to Not Found page and update build script in package.json

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+export const metadata = {
+    title: 'Not Found',
+    description: 'This page could not be found.',
+};
 
 const NotFound = () => {
     return (
         <div className='text-4xl font-extrabold w-screen pt-20 grid place-items-center'>
-            <h1>
-                404 - Page Not Found
-            </h1>
+            <h1>404 - Page Not Found</h1>
         </div>
-    )
-}
+    );
+};
 
-export default NotFound
+export default NotFound;
